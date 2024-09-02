2a8aee55e36f8237e040dbe2a26bcd0a28fee684
Aman-Pailwan <apailwan@databahn.ai>
2024-09-02 18:30:53 +0530

    Kadanes Algorithm

--- Algorithm/KadanesAlgorithm.py
+++ Algorithm/KadanesAlgorithm.py
@@ -1,9 +1,9 @@
 def kadanes(nums : list[int]) -> int:
     maxSum = nums[0]
     currSum = 0
-
+    # We Consider the currentSum only if it greater then 0(positive)
     for n in nums:
-        currSum = max(currSum , 0)
+        currSum = max(currSum , 0) # We avoid negative values as they reduce our max sum
         currSum += n
         maxSum = max(maxSum , currSum)
     return maxSum
